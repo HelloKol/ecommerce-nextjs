6a265d8b1f7245cdf5f96c1bc5f78ae0c4b16887
shehab emon <shehabhasan2020@gmail.com>
2022-04-01 00:02:19 +0100

    collection page vertical snap scroll infinite working

--- pages/collections/index.tsx
+++ pages/collections/index.tsx
@@ -1,82 +1,73 @@
 import { NextPage } from "next";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useSpringCarousel } from "react-spring-carousel";
 import styles from "./styles.module.scss";
+import imgTest from "../../public/static/media/about-girl.jpg";
 
-const arr = [
+const mockedItems = [
   {
-    id: 0,
+    id: "0",
     title: "collection 1",
     image: "https://picsum.photos/id/1/1920/1920",
   },
   {
-    id: 1,
+    id: "1",
     title: "collection 2",
     image: "https://picsum.photos/id/2/1920/1920",
   },
   {
-    id: 2,
+    id: "2",
     title: "collection 3",
     image: "https://picsum.photos/id/3/1920/1920",
   },
   {
-    id: 3,
+    id: "3",
     title: "collection 4",
     image: "https://picsum.photos/id/4/1920/1920",
   },
   {
-    id: 4,
+    id: "4",
     title: "collection 5",
     image: "https://picsum.photos/id/5/1920/1920",
   },
   {
-    id: 5,
+    id: " 5",
     title: "collection 6",
     image: "https://picsum.photos/id/6/1920/1920",
   },
 ];
 
 const Collections: NextPage = () => {
-  const [isHovered, setIsHovered] = useState({} as any);
-
-  const handleMouseEnter = (index: any) => {
-    setIsHovered((prev: any) => {
-      return { ...prev, [index]: true };
-    });
-  };
-  const handleMouseLeave = (index: any) => {
-    setIsHovered((prev: any) => {
-      return { ...prev, [index]: false };
-    });
-  };
-  const renderCollections = () =>
-    arr.map((item, index) => {
-      const { id, title, image } = item;
-      return (
-        <li
-          className={`${styles.collectionItems}`}
-          key={index}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseLeave={() => handleMouseLeave(index)}
-        >
+  // carousel
+  const { carouselFragment } = useSpringCarousel({
+    withLoop: true,
+    carouselSlideAxis: "y",
+    items: mockedItems.map((item) => ({
+      id: item.id,
+      renderItem: (
+        <div className={styles.items} key={item.id}>
           <div className={styles.imgWrapper}>
-            <img
-              className={`${styles.img} ${
-                isHovered[index] ? styles.imgShow : styles.imgHide
-              }`}
-              src={image}
-              alt=""
+            <Image
+              src={imgTest}
+              alt={item.title}
+              width={500}
+              height={500}
+              objectFit="contain"
+              quality={100}
+              className={styles.img}
             />
+            {/* <h1 className={styles.title}>{item.title}</h1> */}
           </div>
-          <h1 className={styles.title}>{title}</h1>
-          <hr />
-        </li>
-      );
-    });
+        </div>
+      ),
+    })),
+  });
 
   return (
     <section className={styles.collectionSection}>
       <div className={styles.container}>
-        <ul className={styles.collectionList}>{renderCollections()}</ul>
+        <div className={styles.collectionList}>{carouselFragment}</div>
       </div>
     </section>
   );
