5353640cf1a7ac464e5ee2677fb4933a31974bb9
shehab emon <shehabhasan2020@gmail.com>
2022-04-04 21:01:36 +0100

    play working latest

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,28 +1,47 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const index = () => {
   const waveformRef = useRef(null);
+  let [isPlaying, setIsPlaying] = useState(true);
+  let [waveSurfer, setWaveSurfer] = useState(null) as any;
+
   useEffect(() => {
     if (typeof self != "undefined" || typeof self != undefined) {
       const WaveSurfer = require("wavesurfer.js");
       if (waveformRef.current) {
-        const wavesurfer = WaveSurfer.create({
-          container: waveformRef.current,
-        });
-        wavesurfer.load(
-          "https://freesound.org/data/previews/462/462807_8386274-lq.mp3"
+        setWaveSurfer(
+          WaveSurfer.create({
+            container: waveformRef.current,
+            waveColor: "violet",
+          })
         );
-        wavesurfer.on("ready", function () {
-          wavesurfer.play();
-        });
       }
     }
   }, []);
 
+  useEffect(() => {
+    if (waveSurfer) {
+      waveSurfer.load("/static/mf.mp3");
+      waveSurfer.on("ready", function () {
+        waveSurfer.play();
+      });
+    }
+    return () => {
+      waveSurfer?.destroy();
+    };
+  }, [waveSurfer]);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+    waveSurfer.playPause();
+  };
+
   return (
     <div style={{ marginTop: "15rem" }}>
-      <h1>Hello World</h1>
       <div ref={waveformRef} />
+      <button onClick={() => togglePlayPause()}>
+        {isPlaying ? "pause" : "play"}
+      </button>
     </div>
   );
 };
